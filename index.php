fc0bfd0a4f2d33d813fbc9de10b931f2a4e97840
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-16 18:04:28 +0100

    paragraph text and length printed

--- index.php
+++ index.php
@@ -11,10 +11,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
+
+    <header class="text-center py-3">
+        <h1>NON SI PUO' PIU' DIRE NULLA!</h1>
+        <h2>La censura fai da te</h2>
+    </header>
+
     <main>
-        <form action="script.php" method="GET" class="container d-flex flex-column gap-3 py-5">
-            <textarea class="p-3" name="paragraph" placeholder="Inserisci del testo..."></textarea>
-            <input class="p-3" type="text" placeholder="Inserisci la parola da censurare..." name="censored_word">
+        <form action="script.php" method="GET" class="container d-flex flex-column gap-3">
+            <textarea class="p-3" name="paragraph" placeholder="Inserisci ciò che non ci dicono..."></textarea>
+            <input class="p-3" type="text" placeholder="Inserisci la censura dei poteri forti..." name="censored_word">
             <button>Send!</button>
         </form>
     </main>
--- script.php
+++ script.php
@@ -3,8 +3,9 @@
 $paragraph = $_GET['paragraph'];
 $censored_word = $_GET['censored_word'];
 $asterisks = '***';
+$paragraph_length = strlen($paragraph);
 
-echo $paragraph, $censored_word;
+// echo 'Il testo del paragrafo è '.$paragraph.' mentre la parola censurata è '.$censored_word;
 
 ?>
 
@@ -19,7 +20,18 @@ echo $paragraph, $censored_word;
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <p>Ciao</p>
-    <a href="index.php">Vai a Index!</a>
+    <header class="text-center py-3">
+        <h1>NON SI PUO' PIU' DIRE NULLA!</h1>
+        <h2>La censura fai da te</h2>
+    </header>
+    <main>
+        <div class="container">
+            <p>Il testo originale che i <i>poteri forti</i> vogliono censurare è: <b><?= $paragraph ?></b></p>
+            <p>La lunghezza di questo testo è pari a: <b><?= $paragraph_length ?> caratteri</b></p>
+        </div>
+    </main>
+    <footer class="text-center">
+        <a href="index.php">Back to Main Page!</a>
+    </footer>
 </body>
 </html>
